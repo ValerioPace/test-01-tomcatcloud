27874f542118dc2d1904c8fc2396aa0691f26808
Pace Valerio <v.pace@almaviva.it>
2018-06-10 20:32:25 +0200

    initial commit test-01 tomcat cloud minishift

---
+++ src/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
@@ -0,0 +1,26 @@
+package it.gov.mef.cloudify.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import it.gov.mef.cloudify.model.Ente;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EntityScan(basePackageClasses = Ente.class)
+public class NoiPAServletInitializer extends SpringBootServletInitializer {
+ 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NoiPAServletInitializer.class);
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(NoiPAServletInitializer.class, args);
+    }
+ 
+}
