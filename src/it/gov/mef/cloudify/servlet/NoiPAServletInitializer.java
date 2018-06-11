60daffac36c0763c8fa6a071180da0de439306a6
Pace Valerio <v.pace@almaviva.it>
2018-06-11 14:19:30 +0200

    add Hibernate LocalContainer Entity Manager Factory pattern

--- src/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
+++ src/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
@@ -15,6 +15,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import it.gov.mef.cloudify.model.Ente;
@@ -44,6 +48,24 @@ public class NoiPAServletInitializer extends SpringBootServletInitializer {
     public DataSource jndiDataSource() {
     	JndiDataSourceLookup lookup = new JndiDataSourceLookup();
     	return lookup.getDataSource(primaryJndiName);  	
+    }
+    
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+        lef.setDataSource(dataSource);
+        lef.setJpaVendorAdapter(jpaVendorAdapter);
+        lef.setPackagesToScan("it.gov.mef.cloudify.model");
+        return lef;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(false);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        return hibernateJpaVendorAdapter;
     }
 	
     @Override
