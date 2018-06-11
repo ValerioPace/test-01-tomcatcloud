c479ee8b0c16c0deae4d7e6cf6ff1fe4a4e0b781
Pace Valerio <v.pace@almaviva.it>
2018-06-11 12:35:22 +0200

    commit exclude Hikari CP dependency get jndi ref from spring factory bean and tomcat server context

--- src/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
+++ src/it/gov/mef/cloudify/servlet/NoiPAServletInitializer.java
@@ -1,10 +1,16 @@
 package it.gov.mef.cloudify.servlet;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import it.gov.mef.cloudify.model.Ente;
@@ -14,6 +20,20 @@ import it.gov.mef.cloudify.model.Ente;
 @EntityScan(basePackageClasses = Ente.class)
 public class NoiPAServletInitializer extends SpringBootServletInitializer {
  
+	
+    @Value("${spring.datasource.jndi-name}")
+	private String primaryJndiName;
+    
+    @Value("${spring.datasource.driver-class}")
+    private String driverClass;
+    
+    @Primary
+    @Bean(destroyMethod = "", name="dataSource")
+    public DataSource jndiDataSource() {
+    	JndiDataSourceLookup lookup = new JndiDataSourceLookup();
+    	return lookup.getDataSource(primaryJndiName);  	
+    }
+	
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(NoiPAServletInitializer.class);
